0359624600b64505ffc987b7d3f9ef1e7faefcb3
AZIZ <a.matyaqubov1471@gmail.com>
2022-01-31 11:37:08 +0100

    solve exercise c

--- answers.js
+++ answers.js
@@ -27,6 +27,8 @@ let sum = 10 + 20
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+random = Math.floor(Math.random() * 20)
+
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
